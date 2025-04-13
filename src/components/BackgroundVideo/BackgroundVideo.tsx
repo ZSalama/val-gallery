1592f38efda9855c69ced54bd281ab4e3c97657e
Zack Salama <zacksalama@gmail.com>
2025-04-13 19:21:30 -0400

    background video fix ipad

--- src/components/BackgroundVideo/BackgroundVideo.tsx
+++ src/components/BackgroundVideo/BackgroundVideo.tsx
@@ -1,6 +1,19 @@
+// components/BackgroundVideo.tsx
+'use client'
+import { useEffect, useRef } from 'react'
+
 export default function BackgroundVideo() {
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    useEffect(() => {
+        if (videoRef.current) {
+            videoRef.current.playbackRate = 0.7
+        }
+    }, [])
+
     return (
         <video
+            ref={videoRef}
             autoPlay
             muted
             loop
@@ -8,7 +21,7 @@ export default function BackgroundVideo() {
             className='fixed top-0 left-0 w-full h-full object-cover z-[-1]'
         >
             <source
-                src='https://d2oeo8w8j25w98.cloudfront.net/Star_Background2.webm'
+                src='https://d2oeo8w8j25w98.cloudfront.net/star_background3.mp4'
                 type='video/webm'
             />
         </video>
